3eb8e14861608b711ff03d1505c04153fd15ccea
George Stagas <gstagas@gmail.com>
2021-07-08 20:24:18 +0300

    auto adjust piano octaves

--- components/piano.js
+++ components/piano.js
@@ -3,102 +3,108 @@ import { callback, create, effect } from '../dist/index.js'
 export default create({
   class: 'piano',
 
-  attrs: {},
+  attrs: { octaves: 3 },
 
   props: { notePressed: Number },
 
-  pins: { keyboard: '[part=keyboard]' },
+  pins: { outer: '#outer', keyboard: '[part=keyboard]' },
 
   component() {
-    this.html = `
-      <style>
-        :host {
-          --pressed-white: #e44;
-          --pressed-black: #e44;
-        }
+    effect(() => {
+      this.html = `
+        <style>
+          :host {
+            --pressed-white: #e44;
+            --pressed-black: #e44;
+          }
 
-        #outer {
-          user-select: none;
-          height: 100%;
-          width: 100%;
-          display: inline-flex;
-          margin: auto;
-          justify-content: center;
-          align-items: center;
-        }
+          #outer {
+            user-select: none;
+            height: 100%;
+            width: 100%;
+            display: inline-flex;
+            margin: auto;
+            justify-content: center;
+            align-items: center;
+          }
 
-        .key {
-          box-sizing: border-box;
-          float: left;
-          position: relative;
-          color: #999;
-          display: flex;
-          flex-flow: row nowrap;
-          flex: 1;
-          align-items:flex-end;
-          white-space: nowrap;
-          text-transform: uppercase;
-          font-family: sans-serif;
-          font-size: 6pt;
-          padding: 3px;
-        }
+          .key {
+            box-sizing: border-box;
+            float: left;
+            position: relative;
+            color: #999;
+            display: flex;
+            flex-flow: row nowrap;
+            flex: 1;
+            align-items:flex-end;
+            white-space: nowrap;
+            text-transform: uppercase;
+            font-family: sans-serif;
+            font-size: 6pt;
+            padding: 3px;
+          }
 
-        .white {
-          height: 15vw;
-          flex: 1;
-          z-index: 1;
-          background-color: #CBCBCB;
-        }
+          .white {
+            height: 15vw;
+            flex: 10;
+            z-index: 1;
+            background-color: #CBCBCB;
+          }
 
-        .black {
-          flex: 0.8;
-          height: 9.2vw;
-          width: calc(100vw/10*0.5);
-          z-index: 2;
-          background-color: #101010;
-        }
+          .black {
+            z-index: 2;
+            flex: 5;
+            height: 9.2vw;
+            background-color: #101010;
+          }
 
-        .a, .b, .d, .e, .g, .black {
-          margin: 0 0 0 -3vw;
-        }
+          .black {
+            margin: 0 -${[4, 2, 1.5, 1][this.octaves - 1]}vw;
+          }
 
-        .pressed.white {
-          background: var(--pressed-white) !important;
-        }
+          .pressed.white {
+            background: var(--pressed-white) !important;
+          }
 
-        .pressed.black {
-          background: var(--pressed-black) !important;
-        }
+          .pressed.black {
+            background: var(--pressed-black) !important;
+          }
 
-        #keyboard {
-          display: flex;
-          flex-flow: row nowrap;
-          flex: 1;
-        }
+          #keyboard {
+            display: flex;
+            flex-flow: row nowrap;
+            flex: 1;
+          }
+        </style>
 
-        #keyboard > .note:last-child {
-          border-right: 0.12vw solid #AAAAAA;
-        }
-      </style>
-
-      <div id="outer">
-        <div id="keyboard" part="keyboard">
-          ${
-      Array(12 * 2).fill(0).map((_, i) => {
-        const ii = i % 12
-        const bw = 'wbwbwwbwbwbw'[ii] === 'b'
-        const nt = 'ccddeffggaab'[ii]
-        const sh = '-s-s--s-s-s-'[ii] === 's'
-        return `<div data-note="${i}" part="${
-          bw ? 'piano-black' : 'piano-white'
-        }" class="note key ${bw ? 'black' : 'white'} ${nt}">${nt}${
-          sh ? '#' : ''
-        }</div>`
-      }).join('')
-    }
+        <div id="outer">
+          <div id="keyboard" part="keyboard">
+            ${
+        Array(12 * this.octaves).fill(0).map((_, i) => {
+          const ii = i % 12
+          const bw = 'wbwbwwbwbwbw'[ii] === 'b'
+          const nt = 'ccddeffggaab'[ii]
+          const sh = '-s-s--s-s-s-'[ii] === 's'
+          return `<div data-note="${i}" part="${
+            bw ? 'piano-black' : 'piano-white'
+          }" class="note key ${bw ? 'black' : 'white'} ${nt}">${nt}${
+            sh ? '#' : ''
+          }</div>`
+        }).join('')
+      }
+          </div>
         </div>
-      </div>
-    `
+      `
+    }, this.octaves)
+
+    effect(() => {
+      const resizeObserver = new ResizeObserver(callback(() => {
+        this.octaves = Math.round(
+          (this.outer.getBoundingClientRect().width / 32) / 12,
+        )
+      }))
+      resizeObserver.observe(this.outer)
+    }, this.outer)
 
     effect(() => {
       this.keyboard.addEventListener(
